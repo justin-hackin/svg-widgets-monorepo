bc003f7c0a627bd06c27df83b5cb27c60c79c208
Justin Barca <justin.barca@points.com>
2019-10-26 10:31:48 -0400

    refactor pattern

--- src/components/SVGViewer.jsx
+++ src/components/SVGViewer.jsx
@@ -3,34 +3,24 @@ import React, { useState } from 'react';
 
 import { ReactSVGPanZoom, INITIAL_VALUE, TOOL_PAN } from 'react-svg-pan-zoom';
 import { PyramidNet } from './PyramidNet';
+import { GridPattern } from './GridPattern';
 
 const svgDimensions = { width: 1024, height: 960 };
-const gridPattern = () => (
-  <defs>
-    <pattern id="grid-pattern" patternUnits="userSpaceOnUse" x="0" y="0" width="100" height="100">
-      {/* eslint-disable-next-line max-len */}
-      <g fill="#fff">
-        <rect x={0} y={0} width={100} height={100} fill="#cecece" />
-        {/* eslint-disable-next-line max-len,react/jsx-first-prop-new-line,react/jsx-max-props-per-line */}
-        <path opacity=".4" d="M96 95h4v1h-4v4h-1v-4h-9v4h-1v-4h-9v4h-1v-4h-9v4h-1v-4h-9v4h-1v-4h-9v4h-1v-4h-9v4h-1v-4h-9v4h-1v-4h-9v4h-1v-4H0v-1h15v-9H0v-1h15v-9H0v-1h15v-9H0v-1h15v-9H0v-1h15v-9H0v-1h15v-9H0v-1h15v-9H0v-1h15v-9H0v-1h15V0h1v15h9V0h1v15h9V0h1v15h9V0h1v15h9V0h1v15h9V0h1v15h9V0h1v15h9V0h1v15h9V0h1v15h4v1h-4v9h4v1h-4v9h4v1h-4v9h4v1h-4v9h4v1h-4v9h4v1h-4v9h4v1h-4v9h4v1h-4v9zm-1 0v-9h-9v9h9zm-10 0v-9h-9v9h9zm-10 0v-9h-9v9h9zm-10 0v-9h-9v9h9zm-10 0v-9h-9v9h9zm-10 0v-9h-9v9h9zm-10 0v-9h-9v9h9zm-10 0v-9h-9v9h9zm-9-10h9v-9h-9v9zm10 0h9v-9h-9v9zm10 0h9v-9h-9v9zm10 0h9v-9h-9v9zm10 0h9v-9h-9v9zm10 0h9v-9h-9v9zm10 0h9v-9h-9v9zm10 0h9v-9h-9v9zm9-10v-9h-9v9h9zm-10 0v-9h-9v9h9zm-10 0v-9h-9v9h9zm-10 0v-9h-9v9h9zm-10 0v-9h-9v9h9zm-10 0v-9h-9v9h9zm-10 0v-9h-9v9h9zm-10 0v-9h-9v9h9zm-9-10h9v-9h-9v9zm10 0h9v-9h-9v9zm10 0h9v-9h-9v9zm10 0h9v-9h-9v9zm10 0h9v-9h-9v9zm10 0h9v-9h-9v9zm10 0h9v-9h-9v9zm10 0h9v-9h-9v9zm9-10v-9h-9v9h9zm-10 0v-9h-9v9h9zm-10 0v-9h-9v9h9zm-10 0v-9h-9v9h9zm-10 0v-9h-9v9h9zm-10 0v-9h-9v9h9zm-10 0v-9h-9v9h9zm-10 0v-9h-9v9h9zm-9-10h9v-9h-9v9zm10 0h9v-9h-9v9zm10 0h9v-9h-9v9zm10 0h9v-9h-9v9zm10 0h9v-9h-9v9zm10 0h9v-9h-9v9zm10 0h9v-9h-9v9zm10 0h9v-9h-9v9zm9-10v-9h-9v9h9zm-10 0v-9h-9v9h9zm-10 0v-9h-9v9h9zm-10 0v-9h-9v9h9zm-10 0v-9h-9v9h9zm-10 0v-9h-9v9h9zm-10 0v-9h-9v9h9zm-10 0v-9h-9v9h9zm-9-10h9v-9h-9v9zm10 0h9v-9h-9v9zm10 0h9v-9h-9v9zm10 0h9v-9h-9v9zm10 0h9v-9h-9v9zm10 0h9v-9h-9v9zm10 0h9v-9h-9v9zm10 0h9v-9h-9v9z" />
-        <path opacity=".8" d="M6 5V0H5v5H0v1h5v94h1V6h94V5H6z" />
-      </g>
-    </pattern>
-  </defs>
-);
+
 export function SVGViewer() {
   // TODO: make this work
   // const [value = INITIAL_VALUE, setValue] = useQueryParam('ReactSVGPanZoomValue', JsonParam);
   // const [tool = TOOL_PAN, setTool] = useQueryParam('ReactSVGPanZoomTool', StringParam);
   const [value, setValue] = useState(INITIAL_VALUE);
   const [tool, setTool] = useState(TOOL_PAN);
+  const patternId = 'grid-pattern';
   return (
     <div>
       <ReactSVGPanZoom
         value={value}
         {...svgDimensions}
         background="#454545"
-        SVGBackground="url(#grid-pattern)"
+        SVGBackground={`url(#${patternId})`}
         tool={tool}
         onChangeValue={(val) => {
           setValue(val);
@@ -39,7 +29,7 @@ export function SVGViewer() {
         id="viewer"
       >
         <svg {...svgDimensions}>
-          {gridPattern()}
+          <GridPattern patternId={patternId} />
           <g transform="translate(300, 300) scale(4, 4) ">
             <PyramidNet netSpec={{ faceEdgeLengths: [40, 30, 50], faceCount: 4 }} />
           </g>
