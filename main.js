39debabbc987ae7c43ae7ed607b285484dc56923
Justin Barca <justinbarca@gmail.com>
2019-11-22 21:52:51 -0500

    electron-ification

---
+++ main.js
@@ -0,0 +1,57 @@
+// Basic init from https://github.com/aimerib/electron-react-parcel/
+// eslint-disable-next-line import/no-extraneous-dependencies
+const electron = require('electron');
+
+const { app, BrowserWindow } = electron;
+const path = require('path');
+const url = require('url');
+
+const args = process.argv.slice(1);
+const serve = args.some((val) => val === '--start-dev');
+
+// Let electron reloads by itself when webpack watches changes in ./app/
+if (serve) {
+  // eslint-disable-next-line global-require,import/no-extraneous-dependencies
+  require('electron-reload')(__dirname);
+}
+
+// To avoid being garbage collected
+let mainWindow;
+
+app.on('ready', () => {
+  // eslint-disable-next-line no-shadow
+  let mainWindow = new BrowserWindow({ width: 800, height: 600 });
+
+  const startUrl = serve ? 'http://localhost:1234' : url.format({
+    pathname: path.join(__dirname, './build/index.html'),
+    protocol: 'file:',
+    slashes: true,
+  });
+
+  mainWindow.loadURL(startUrl);
+
+  mainWindow.on('closed', () => {
+    // Dereference the window object, usually you would store windows
+    // in an array if your app supports multi windows, this is the time
+    // when you should delete the corresponding element.
+    mainWindow = null;
+  });
+});
+
+// Quit when all windows are closed.
+app.on('window-all-closed', () => {
+  // On OS X it is common for applications and their menu bar
+  // to stay active until the user quits explicitly with Cmd + Q
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
+});
+
+app.on('activate', () => {
+  // On OS X it's common to re-create a window in the app when the
+  // dock icon is clicked and there are no other windows open.
+  if (mainWindow === null) {
+    // eslint-disable-next-line no-undef
+    createWindow();
+  }
+});
