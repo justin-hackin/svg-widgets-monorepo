bc5fad84d65b56cfaecf005ad77c044f2fa5a75d
Justin Barca <justinbarca@gmail.com>
2019-11-23 16:46:54 -0500

    separate main and render into separate builds
    as per https://github.com/shamofu/electron-react-parcel-boilerplate

---
+++ app/main/main.ts
@@ -0,0 +1,42 @@
+// Basic init from https://github.com/aimerib/electron-react-parcel/
+// eslint-disable-next-line import/no-extraneous-dependencies
+const { app, BrowserWindow, ipcMain } = require('electron');
+const { format } = require('url');
+const isDev = require('electron-is-dev');
+const fs = require('fs');
+const { promisify } = require('util');
+const { resolve } = require('app-root-path');
+
+const writeFileAsync = promisify(fs.writeFile);
+
+const saveStringToDisk = (filePath, data) => writeFileAsync(filePath, data);
+
+ipcMain.on('save-string', (e, filePath, string) => {
+  saveStringToDisk(filePath, string);
+});
+
+app.on('ready', async () => {
+  const mainWindow = new BrowserWindow({
+    width: 800,
+    height: 600,
+    show: false,
+  });
+
+  mainWindow.once('ready-to-show', () => {
+    mainWindow.show();
+    // if (isDev) { mainWindow.webContents.openDevTools(); }
+  });
+
+  const devPath = 'http://localhost:1124';
+  const prodPath = format({
+    pathname: resolve('app/renderer/.parcel/production/index.html'),
+    protocol: 'file:',
+    slashes: true,
+  });
+  const url = isDev ? devPath : prodPath;
+
+  mainWindow.setMenu(null);
+  mainWindow.loadURL(url);
+});
+
+app.on('window-all-closed', app.quit);
