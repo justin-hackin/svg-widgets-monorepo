7156ed2fc67721428d8702f0e22ebc10c6404592
Justin Barca <justin.barca@points.com>
2020-01-05 15:08:52 -0500

    fix transform origin point placement at center

---
+++ app/renderer/die-line-viewer/components/ResizeDetector.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactResizeDetector from 'react-resize-detector';
+import { isValidNumber } from '../util/geom';
+
+export const ResizeDetector = (props) => (
+  <ReactResizeDetector handleWidth handleHeight>
+    {({ width, height }) => ((!isValidNumber(width) || !isValidNumber(height))
+      ? (<div />)
+      : props.children({ width, height }))}
+  </ReactResizeDetector>
+);
