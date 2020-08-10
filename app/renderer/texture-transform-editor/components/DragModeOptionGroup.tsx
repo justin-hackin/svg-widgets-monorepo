7705f6bba0a21c1d5fa368cba0da1f6fde75f14c
Justin Barca <justinbarca@gmail.com>
2020-08-09 22:04:32 -0400

    use modifier keys to set drag mode, with button group as indicator

--- app/renderer/texture-transform-editor/components/DragModeOptionGroup.tsx
+++ app/renderer/texture-transform-editor/components/DragModeOptionGroup.tsx
@@ -5,7 +5,6 @@ import CachedIcon from '@material-ui/icons/Cached';
 import OpenWithIcon from '@material-ui/icons/OpenWith';
 import AspectRatioIcon from '@material-ui/icons/AspectRatio';
 import ZoomOutMapIcon from '@material-ui/icons/ZoomOutMap';
-import { useHotkeys } from 'react-hotkeys-hook';
 
 import * as React from 'react';
 
@@ -16,43 +15,26 @@ export const DRAG_MODES = {
   SCALE_VIEW: 'scale view',
 };
 
-export const DragModeOptionsGroup = ({ dragMode, setDragMode }) => {
-  useHotkeys('shift+r', () => { setDragMode(DRAG_MODES.ROTATE); });
-  useHotkeys('shift+d', () => { setDragMode(DRAG_MODES.TRANSLATE); });
-  useHotkeys('shift+s', () => { setDragMode(DRAG_MODES.SCALE_TEXTURE); });
-  useHotkeys('shift+v', () => { setDragMode(DRAG_MODES.SCALE_VIEW); });
-
+export const DragModeOptionsGroup = ({ dragMode }) => {
   return (
     <ToggleButtonGroup
       value={dragMode}
       style={{ height: 'fit-content', margin: '0.5em' }}
       exclusive
-      onChange={(_, newDragMode) => {
-        setDragMode(newDragMode);
-      }}
       aria-label="drag mode"
     >
       {/* translate is too technical for end user */}
-      <ToggleButton value={DRAG_MODES.TRANSLATE} aria-label={DRAG_MODES.TRANSLATE}>
-        {/* Tooltips should be around whole button but this breaks the active style */}
-        <Tooltip title="drag (⬆️ + d)">
-          <OpenWithIcon />
-        </Tooltip>
+      <ToggleButton disabled value={DRAG_MODES.TRANSLATE} aria-label={DRAG_MODES.TRANSLATE}>
+        <OpenWithIcon />
       </ToggleButton>
-      <ToggleButton value={DRAG_MODES.ROTATE} aria-label={DRAG_MODES.ROTATE}>
-        <Tooltip title="rotate (⬆️ + r)">
-          <CachedIcon />
-        </Tooltip>
+      <ToggleButton disabled value={DRAG_MODES.ROTATE} aria-label={DRAG_MODES.ROTATE}>
+        <CachedIcon />
       </ToggleButton>
-      <ToggleButton value={DRAG_MODES.SCALE_TEXTURE} aria-label={DRAG_MODES.SCALE_TEXTURE}>
-        <Tooltip title="scale texture (⬆️ + s)">
-          <ZoomOutMapIcon />
-        </Tooltip>
+      <ToggleButton disabled value={DRAG_MODES.SCALE_TEXTURE} aria-label={DRAG_MODES.SCALE_TEXTURE}>
+        <ZoomOutMapIcon />
       </ToggleButton>
-      <ToggleButton value={DRAG_MODES.SCALE_VIEW} aria-label={DRAG_MODES.SCALE_VIEW}>
-        <Tooltip title="scale view (⬆️ + v)">
-          <AspectRatioIcon />
-        </Tooltip>
+      <ToggleButton disabled value={DRAG_MODES.SCALE_VIEW} aria-label={DRAG_MODES.SCALE_VIEW}>
+        <AspectRatioIcon />
       </ToggleButton>
     </ToggleButtonGroup>
   );
