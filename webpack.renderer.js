658993444727e9b2d4b292ccf1904ccba38f909a
Justin Barca <justinbarca@gmail.com>
2020-10-19 11:50:05 -0400

    exclude mobx-devtools on production

--- webpack.renderer.js
+++ webpack.renderer.js
@@ -1,9 +1,27 @@
 /* eslint-env node */
+// eslint-disable-next-line import/no-extraneous-dependencies
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 // eslint-disable-next-line import/no-extraneous-dependencies
 const { merge } = require('webpack-merge');
 
 module.exports = function (config) {
+  const htmlPlugin = config.plugins.find((plugin) => plugin instanceof HtmlWebpackPlugin);
+  htmlPlugin.options.template = 'src/renderer/index.ejs';
+  const { templateParameters } = htmlPlugin.options;
+
+  htmlPlugin.options.templateParameters = (...params) => merge(
+    {
+      isDev: process.env.NODE_ENV !== 'production',
+      electronHeadScript: `<script>
+      ${htmlPlugin.nodeModules == null
+    ? ''
+    : `require("module").globalPaths.push("${htmlPlugin.nodeModules.replace(/\\/g, '/')}")`}
+      require("source-map-support/source-map-support.js").install()
+    </script>`,
+    }, templateParameters(...params),
+  );
+
   return merge(config, {
     module: {
       rules: [
