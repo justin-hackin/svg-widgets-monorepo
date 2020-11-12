f9c094da672b5ed31deea5ff8f14436e7fc8b6bc
Justin Barca <justinbarca@gmail.com>
2020-11-11 20:39:17 -0500

    improve type safety

--- src/renderer/DielineViewer/util/PathData.ts
+++ src/renderer/DielineViewer/util/PathData.ts
@@ -1,6 +1,4 @@
-import {
-  cloneDeep, isNaN, includes, last,
-} from 'lodash';
+import { cloneDeep, isNaN, includes } from 'lodash';
 // @ts-ignore
 import { Point } from '@flatten-js/core';
 import svgpath from 'svgpath';
@@ -23,71 +21,140 @@ const castToArray = (pt: Coord):PointTuple => {
   return pt;
 };
 
-const BEZIER_COMMAND_CODES = ['Q', 'T', 'C', 'S'];
+
+enum CommandCodes { M='M', L='L', C='C', S='S', Q='Q', T='T', A='A', Z='Z' }
+
+interface DestinationCommand {
+  to: PointTuple
+}
+
+interface MoveCommand extends DestinationCommand {
+  code: CommandCodes.M
+}
+
+interface LineCommand extends DestinationCommand {
+  code: CommandCodes.L
+}
+
+interface CubicBezierCommand extends DestinationCommand {
+  code: CommandCodes.C
+  ctrl1: PointTuple
+  ctrl2: PointTuple
+}
+
+interface SymmetricCubicBezierCommand extends DestinationCommand{
+  code: CommandCodes.S
+  ctrl2: PointTuple
+}
+
+interface QuadraticBezierCommand extends DestinationCommand {
+  code: CommandCodes.Q
+  ctrl1: PointTuple
+}
+
+interface SymmetricQuadraticBezierCommand extends DestinationCommand {
+  code: CommandCodes.T
+}
+
+interface ArcCommand extends DestinationCommand {
+  code: CommandCodes.A
+  rx: number
+  ry: number
+  sweepFlag: boolean
+  largeArcFlag: boolean
+  xAxisRotation: number
+}
+
+interface CloseCommand {
+  code: CommandCodes.Z
+}
+
+type Command = MoveCommand | LineCommand | CubicBezierCommand | SymmetricCubicBezierCommand | QuadraticBezierCommand
+| SymmetricQuadraticBezierCommand | ArcCommand | CloseCommand;
+
+type BezierCommand = QuadraticBezierCommand | SymmetricQuadraticBezierCommand |
+CubicBezierCommand | SymmetricCubicBezierCommand;
+
+const BEZIER_COMMAND_CODES = [CommandCodes.Q, CommandCodes.T, CommandCodes.C, CommandCodes.S];
+type OnlyToParamCommand = LineCommand | MoveCommand | SymmetricQuadraticBezierCommand;
+const hasOnlyToParam = (command: Command): command is OnlyToParamCommand => includes(
+  [CommandCodes.L, CommandCodes.M, CommandCodes.T],
+  (command as OnlyToParamCommand).code,
+);
+
+const isBezierCommand = (command: Command): command is BezierCommand => includes(
+  BEZIER_COMMAND_CODES,
+  (command as BezierCommand).code,
+);
+
+// TODO: how to make this custom type guard, return type "command is DestinationCommand" does not work
+// TODO: remove casting in getDestinationPoints
+const isDestinationCommand = (command: Command): boolean => !!((command as DestinationCommand).to);
 
 const commandToString = (command) => {
-  if (command.code === 'Z') { return command.code; }
-  if (includes(['L', 'M', 'T'], command.code)) {
+  if (command.code === CommandCodes.Z) { return command.code; }
+  if (hasOnlyToParam(command)) {
     return `${command.code} ${command.to.join(',')}`;
   }
-  if (command.code === 'Q') {
+  if (command.code === CommandCodes.Q) {
     return `${command.code} ${command.ctrl1.join(',')} ${command.to.join(',')}`;
   }
-  if (command.code === 'C') {
+  if (command.code === CommandCodes.C) {
     return `${command.code} ${command.ctrl1.join(',')} ${command.ctrl2.join(',')} ${command.to.join(',')}`;
   }
-  if (command.code === 'S') {
+  if (command.code === CommandCodes.S) {
     return `${command.code} ${command.ctrl2.join(',')} ${command.to.join(',')}`;
   }
-  if (command.code === 'A') {
-    return `${command.code} ${command.radius.join(',')} ${command.xAxisRotation} ${
-      command.flags.map((flag) => (flag ? 1 : 0))} ${command.to.join(',')}`;
+  if (command.code === CommandCodes.A) {
+    const booleanToFlag = (flag) => (flag ? 1 : 0);
+    return `${command.code} ${command.rx} ${command.ry} ${command.xAxisRotation} ${
+      booleanToFlag(command.sweepFlag)} ${booleanToFlag(command.largeArcFlag)} ${command.to.join(',')}`;
   }
   throw new Error('Unrecognized command code');
 };
 
-export const COMMAND_FACTORY = {
-  M: (to:Coord) => ({
-    code: 'M',
+export const COMMAND_FACTORY: Record<CommandCodes, (...any)=> Command> = {
+  M: (to:Coord):MoveCommand => ({
+    code: CommandCodes.M,
     to: castToArray(to),
   }),
-  L: (to:Coord) => ({
-    code: 'L',
+  L: (to:Coord):LineCommand => ({
+    code: CommandCodes.L,
     to: castToArray(to),
   }),
-  C: (ctrl1:Coord, ctrl2:Coord, to:Coord) => ({
-    code: 'C',
+  C: (ctrl1:Coord, ctrl2:Coord, to:Coord):CubicBezierCommand => ({
+    code: CommandCodes.C,
     to: castToArray(to),
     ctrl1: castToArray(ctrl1),
     ctrl2: castToArray(ctrl2),
   }),
-  S: (ctrl2, to) => ({
-    code: 'S',
+  S: (ctrl2, to):SymmetricCubicBezierCommand => ({
+    code: CommandCodes.S,
     to: castToArray(to),
     ctrl2: castToArray(ctrl2),
   }),
-  Q: (ctrl1:Coord, to:Coord) => ({
-    code: 'Q',
+  Q: (ctrl1:Coord, to:Coord): QuadraticBezierCommand => ({
+    code: CommandCodes.Q,
     to: castToArray(to),
     ctrl1: castToArray(ctrl1),
   }),
-  T: (to:Coord) => ({
-    code: 'T',
+  T: (to:Coord):SymmetricQuadraticBezierCommand => ({
+    code: CommandCodes.T,
     to: castToArray(to),
   }),
   A: (
     radiusX: number, radiusY: number, xAxisRotation: number, sweepFlag: boolean, largeArcFlag: boolean, to:Coord,
-  ) => ({
-    code: 'A',
+  ):ArcCommand => ({
+    code: CommandCodes.A,
     rx: radiusX,
     ry: radiusY,
-    flags: [sweepFlag, largeArcFlag],
-    to: castToArray(to),
+    sweepFlag,
+    largeArcFlag,
     xAxisRotation,
-
+    to: castToArray(to),
   }),
   Z: () => ({
-    code: 'Z',
+    code: CommandCodes.Z,
   }),
 };
 
@@ -130,7 +197,7 @@ const composeSVG = (commands) => commands.map((command) => commandToString(comma
 
 
 export class PathData {
-  private _commands;
+  private _commands: Command[];
 
   constructor(d?: string) {
     // TODO: check instance type
@@ -151,11 +218,10 @@ export class PathData {
   private _assertLastCommandIsBezier() {
     this._assertLastCommandExists();
     // @ts-ignore
-    const lastCode = last(this._commands).code;
-
-    if (!includes(BEZIER_COMMAND_CODES, lastCode)) {
+    const lastCommand = this._commands[this._commands.length - 1];
+    if (isBezierCommand(lastCommand)) {
       throw new Error(`expected last command to be a bezier (command code one of ${BEZIER_COMMAND_CODES
-      }) but instead saw ${lastCode}`);
+      }) but instead saw ${lastCommand.code}`);
     }
   }
 
@@ -248,7 +314,8 @@ export class PathData {
   }
 
   getDestinationPoints() {
-    return this._commands.filter((cmd) => cmd.to !== undefined).map((cmd) => cmd.to);
+    return this._commands.filter((cmd) => isDestinationCommand(cmd))
+      .map((cmd) => (cmd as DestinationCommand).to);
   }
 
   // TODO: getControlPoints, getInferredControlPoints
