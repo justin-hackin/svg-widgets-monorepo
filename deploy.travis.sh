5abd10f7dcd7d2846b897e12ebeb2040f264a61f
Justin Barca <justinbarca@gmail.com>
2020-11-21 20:03:38 -0500

    electron-builder docker image can't build Windows with native addons, opt for AppVeyor and try to do linux build directly within travis-ci

--- deploy.travis.sh
+++ deploy.travis.sh
@@ -1,8 +1,8 @@
 #! /bin/bash
+yarn add global cmake-js
 if [ "$TRAVIS_OS_NAME" == osx ]; then
     # deploy on mac
     yarn release
 else
-    # deploy on windows and linux
-    docker run --rm -e GH_TOKEN -v "${PWD}":/project -v ~/.cache/electron:/root/.cache/electron -v ~/.cache/electron-builder:/root/.cache/electron-builder justinbarca/electron-builder-wine-lib2geom:1 /bin/bash -c "yarn global add cmake-js && yarn --link-duplicates --pure-lockfile && yarn release --linux AppImage --win"
+   yarn release --linux AppImage
 fi
