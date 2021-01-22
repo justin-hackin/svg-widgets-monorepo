c18577acaa293ea8d6c8602b1e1f85fcf3cde33a
Justin Barca <justinbarca@gmail.com>
2021-01-22 16:42:00 -0500

    Dielines are contained within Inkscape-compatible layer

--- src/renderer/DielineViewer/components/PyramidNet/PyramidNetSvg/index.tsx
+++ src/renderer/DielineViewer/components/PyramidNet/PyramidNetSvg/index.tsx
@@ -7,6 +7,16 @@ import {
   lineLerp, matrixWithTransformOrigin, radToDeg,
 } from '../../../../common/util/geom';
 
+const DielineGroup = ({ children }) => (
+  <g {...{
+    id: 'dielines',
+    'inkscape:groupmode': 'layer',
+    'inkscape:label': 'Dielines',
+  }}
+  >
+    {children}
+  </g>
+);
 export const PyramidNet = ({
   widgetStore, preferencesStore,
 }:{
@@ -32,67 +42,69 @@ export const PyramidNet = ({
     const CUT_HOLES_ID = 'cut-holes';
     const BASE_TAB_ID = 'base-tab';
     const faceMasterBaseMidpoint = lineLerp(faceBoundaryPoints[1], faceBoundaryPoints[2], 0.5);
-
     return (
-      <g transform={fitInBoundsTransform}>
-        <path d={ascendantEdgeTabs.male.score.getD()} {...scoreProps} />
-        <path d={ascendantEdgeTabs.male.cut.getD()} {...cutProps} />
-        <path d={ascendantEdgeTabs.female.score.getD()} {...scoreProps} />
-        <path d={ascendantEdgeTabs.female.cut.getD()} {...cutProps} />
-        <path d={femaleAscendantFlap.getD()} {...cutProps} />
-        <path d={nonTabbedAscendantScores.getD()} {...scoreProps} />
+      <DielineGroup>
+        <g transform={fitInBoundsTransform}>
+          <path d={ascendantEdgeTabs.male.score.getD()} {...scoreProps} />
+          <path d={ascendantEdgeTabs.male.cut.getD()} {...cutProps} />
+          <path d={ascendantEdgeTabs.female.score.getD()} {...scoreProps} />
+          <path d={ascendantEdgeTabs.female.cut.getD()} {...cutProps} />
+          <path d={femaleAscendantFlap.getD()} {...cutProps} />
+          <path d={nonTabbedAscendantScores.getD()} {...scoreProps} />
 
-        {range(faceCount).map((index) => {
-          const isOdd = !!(index % 2);
-          const xScale = isOdd ? -1 : 1;
-          const asymmetryNudge = isOdd ? faceInteriorAngles[2] - 2 * ((Math.PI / 2) - faceInteriorAngles[0]) : 0;
-          const baseTabRotationRad = -1 * index * faceInteriorAngles[2];
-          const decorationRotationRad = xScale * baseTabRotationRad + asymmetryNudge;
-          const cloneTransformMatrix = (new DOMMatrixReadOnly())
-            .scale(xScale, 1).rotate(radToDeg(decorationRotationRad));
+          {range(faceCount).map((index) => {
+            const isOdd = !!(index % 2);
+            const xScale = isOdd ? -1 : 1;
+            const asymmetryNudge = isOdd ? faceInteriorAngles[2] - 2 * ((Math.PI / 2) - faceInteriorAngles[0]) : 0;
+            const baseTabRotationRad = -1 * index * faceInteriorAngles[2];
+            const decorationRotationRad = xScale * baseTabRotationRad + asymmetryNudge;
+            const cloneTransformMatrix = (new DOMMatrixReadOnly())
+              .scale(xScale, 1).rotate(radToDeg(decorationRotationRad));
 
-          return index === 0
-            ? (
-              <>
-                <g key={index} id={CUT_HOLES_ID} transform={borderInsetFaceHoleTransformMatrix.toString()}>
-                  { texturePathD && (
-                  <path d={texturePathD} transform={pathScaleMatrix.toString()} {...cutProps} />
-                  )}
-                </g>
-                <g id={BASE_TAB_ID}>
-                  <path d={masterBaseTab.cut.getD()} {...cutProps} />
-                  <path d={masterBaseTab.score.getD()} {...scoreProps} />
-                </g>
-              </>
-            ) : (
-              <>
-                <use
-                  key={`${index}-decoration`}
-                  xlinkHref={`#${CUT_HOLES_ID}`}
-                  transform={
+            return index === 0
+              ? (
+                <>
+                  <g key={index} id={CUT_HOLES_ID} transform={borderInsetFaceHoleTransformMatrix.toString()}>
+                    { texturePathD && (
+                    <path d={texturePathD} transform={pathScaleMatrix.toString()} {...cutProps} />
+                    )}
+                  </g>
+                  <g id={BASE_TAB_ID}>
+                    <path d={masterBaseTab.cut.getD()} {...cutProps} />
+                    <path d={masterBaseTab.score.getD()} {...scoreProps} />
+                  </g>
+                </>
+              ) : (
+                <>
+                  <use
+                    key={`${index}-decoration`}
+                    xlinkHref={`#${CUT_HOLES_ID}`}
+                    transform={
                     cloneTransformMatrix.toString()
                   }
-                />
-                <g transform={cloneTransformMatrix.toString()}>
-                  <use
-                    key={`${index}-base-tab`}
-                    xlinkHref={`#${BASE_TAB_ID}`}
-                    transform={matrixWithTransformOrigin(
-                      faceMasterBaseMidpoint,
-                      (new DOMMatrixReadOnly()).scale(isOdd ? -1 : 1, 1),
-                    ).toString()}
                   />
-                </g>
-              </>
-            );
-        })}
-      </g>
+                  <g key={`${index}-base-tab`} transform={cloneTransformMatrix.toString()}>
+                    <use
+                      xlinkHref={`#${BASE_TAB_ID}`}
+                      transform={matrixWithTransformOrigin(
+                        faceMasterBaseMidpoint,
+                        (new DOMMatrixReadOnly()).scale(isOdd ? -1 : 1, 1),
+                      ).toString()}
+                    />
+                  </g>
+                </>
+              );
+          })}
+        </g>
+      </DielineGroup>
     );
   }
   return (
-    <g transform={fitInBoundsTransform}>
-      <path className="score" {...scoreProps} d={score.getD()} />
-      <path className="cut" {...cutProps} d={cut.getD()} />
-    </g>
+    <DielineGroup>
+      <g transform={fitInBoundsTransform}>
+        <path className="score" {...scoreProps} d={score.getD()} />
+        <path className="cut" {...cutProps} d={cut.getD()} />
+      </g>
+    </DielineGroup>
   );
 };
--- src/renderer/DielineViewer/data/SVGWrapper.tsx
+++ src/renderer/DielineViewer/data/SVGWrapper.tsx
@@ -7,9 +7,14 @@ const svgNamespaceAttributes = {
   xmlns: 'http://www.w3.org/2000/svg',
 };
 
+// TODO: leverage coming support for colon syntax for namespaced xml elements
+// https://github.com/microsoft/TypeScript/pull/37421/commits/83c6814771cd67555bd01a9e8fc66d57276e83ca
+
 const SodipodiNamedview = namespacedElementFactory('sodipodi:namedview');
 const namedviewAttributes = {
   pagecolor: '#2d2d2d',
+  'window-maximized': '1',
+  'current-layer': 'dielines',
 };
 
 export const SVGWrapper = ({ children, ...rest }) => (
