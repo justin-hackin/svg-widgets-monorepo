589a5d57ac6214ce40e23a174555cc6402a5bef2
Justin Barca <justin.barca@protonmail.com>
2021-01-25 15:04:42 -0500

    fix @typescript-eslint/eslint-plugin upgrade broke airbnb style guide
    see https://stackoverflow.com/a/65404192

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,13 +1,11 @@
 module.exports = {
   extends: 'airbnb-typescript',
-  // the following packages are required as peer dependencies for this config:
-  // eslint-plugin-import eslint-plugin-jsx-a11y eslint-plugin-react eslint-plugin-react-hooks @typescript-eslint/eslint-plugin
-  // see https://github.com/iamturns/eslint-config-airbnb-typescript
+  // see https://github.com/iamturns/eslint-config-airbnb-typescript for peer dependencies
   plugins: ['@typescript-eslint'],
   parser: '@typescript-eslint/parser',
   rules: {
     'import/prefer-default-export': 0,
-    'max-len': ['error', {code: 120}],
+    'max-len': ['error', { code: 120 }],
     'react/jsx-props-no-spreading': 0,
     'react/prop-types': 0,
     'react/no-array-index': 0,
@@ -21,26 +19,26 @@ module.exports = {
     'func-names': 0,
   },
   settings: {
-    'import/extensions': [".js",".jsx",".ts",".tsx"],
+    'import/extensions': ['.js', '.jsx', '.ts', '.tsx'],
     'import/parsers': {
-      '@typescript-eslint/parser': [".ts",".tsx"]
+      '@typescript-eslint/parser': ['.ts', '.tsx'],
     },
 
-    "import/resolver": {
-      "typescript": {
-        "alwaysTryTypes": true,
-        "directory": "."
+    'import/resolver': {
+      typescript: {
+        alwaysTryTypes: true,
+        directory: '.',
       },
     },
-    "react/jsx-filename-extension": [1, {
-      "extensions": [".jsx", ".tsx"]
+    'react/jsx-filename-extension': [1, {
+      extensions: ['.jsx', '.tsx'],
     }],
   },
   parserOptions: {
     ecmaVersion: 11,
-    allowImportExportEverywhere: true
+    allowImportExportEverywhere: true,
   },
   globals: {
-    '__static': true,
-  }
+    __static: true,
+  },
 };
