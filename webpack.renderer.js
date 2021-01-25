0883328a60d02be52c790aa2e036410318d1526d
Justin Barca <justinbarca@gmail.com>
2021-01-25 14:36:52 -0500

    webpack cleanup

--- webpack.renderer.js
+++ webpack.renderer.js
@@ -1,16 +1,11 @@
-// eslint-disable-next-line import/no-extraneous-dependencies
+/* eslint-env node */
 const ReactRefreshWebpackPlugin = require('@pmmmwh/react-refresh-webpack-plugin');
-// eslint-disable-next-line import/no-extraneous-dependencies
-// const ForkTsCheckerWebpackPlugin = require('fork-ts-checker-webpack-plugin');
 
 module.exports = (config) => {
   const isDevelopment = process.env.NODE_ENV !== 'production';
 
   if (isDevelopment) {
     const babelLoader = config.module.rules[0];
-    if (!babelLoader) {
-      throw new Error('could not find babel loader rule based on test is .js');
-    }
     babelLoader.use.options.plugins.push([require.resolve('react-refresh/babel'), {
       skipEnvCheck: true,
     }]);
