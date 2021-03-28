ac8f550aa8eed135f6da0e1b5d7731caf0d2408d
Justin Barca <justin.barca@protonmail.com>
2021-03-28 13:16:15 -0400

    remove web index file, fix web App.tsx

--- src/web/App.tsx
+++ src/web/App.tsx
@@ -1,6 +1,7 @@
-import { ProvidersWrapper } from '../common/components/ProvidersWrapper';
 import { render } from 'react-dom';
 import React from 'react';
+
+import { ProvidersWrapper } from '../common/components/ProvidersWrapper';
 import { TextureEditor } from '../common/components/TextureEditor';
 
 const App = () => (
