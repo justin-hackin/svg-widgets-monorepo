d7594fd07e7cfd53c65c625510fe9ee2dc7277c2
Justin Barca <justinbarca@gmail.com>
2021-09-13 17:05:54 -0400

    one more boilerplate-slayer: TweakableChildrenInputs renders all Tweakable members of a class

--- src/renderer/DielineViewer/widgets/PyramidNet/PyramidNetControlPanel/components/PreferencesControls.tsx
+++ src/renderer/DielineViewer/widgets/PyramidNet/PyramidNetControlPanel/components/PreferencesControls.tsx
@@ -1,53 +1,17 @@
 import React from 'react';
-import { observer } from 'mobx-react';
-import { Button, Divider } from '@material-ui/core';
-import { useWorkspaceMst } from '../../../../models/WorkspaceModel';
-import { PreferencesModel } from '../../../../models/PreferencesModel';
-import { TweakableInput } from '../../../../../../common/keystone-tweakables/material-ui-controls/TweakableInput';
+import {observer} from 'mobx-react';
+import {Button} from '@material-ui/core';
+import {useWorkspaceMst, WorkspaceModel} from '../../../../models/WorkspaceModel';
+import {PreferencesModel} from '../../../../models/PreferencesModel';
+import {TweakableChildrenInputs} from '../../../../../../common/keystone-tweakables/material-ui-controls/TweakableChildrenInputs';
 
 export const PreferencesControls = observer(() => {
-  const workspaceStore = useWorkspaceMst();
-  const { preferences } = workspaceStore;
-  const {
-    displayUnit,
-    documentHeight,
-    documentWidth,
-    useClonesForBaseTabs,
-    useClonesForDecoration,
-    scoreStrokeColor,
-    cutStrokeColor,
-    registrationStrokeColor,
-    printRegistrationType,
-    strokeWidth,
-  } = preferences as PreferencesModel;
+  const workspaceStore: WorkspaceModel = useWorkspaceMst();
+  const { preferences }: { preferences: PreferencesModel} = workspaceStore;
 
   return (
     <>
-      <TweakableInput node={displayUnit} />
-
-      <TweakableInput node={documentWidth} />
-
-      <TweakableInput node={documentHeight} />
-
-      <TweakableInput node={useClonesForBaseTabs} />
-
-      <TweakableInput node={useClonesForDecoration} />
-
-      <TweakableInput node={strokeWidth} />
-
-      <TweakableInput node={scoreStrokeColor} />
-
-      <TweakableInput node={cutStrokeColor} />
-
-      <TweakableInput node={registrationStrokeColor} />
-
-      <Divider />
-
-      <TweakableInput node={printRegistrationType} />
-
-      <TweakableInput node={preferences.registrationPadding} />
-
-      <TweakableInput node={preferences.registrationMarkLength} />
+      <TweakableChildrenInputs parentNode={preferences}/>
 
       <Button
         variant="contained"
