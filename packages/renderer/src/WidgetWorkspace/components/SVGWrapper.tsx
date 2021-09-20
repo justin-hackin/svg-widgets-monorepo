1f97e24e56ddc0e8669de77613920b4e61fd5071
Justin Barca <justin.barca@protonmail.com>
2021-09-20 09:58:19 -0400

    add CC license image/text and RDF to saved documents

--- packages/renderer/src/WidgetWorkspace/components/SVGWrapper.tsx
+++ packages/renderer/src/WidgetWorkspace/components/SVGWrapper.tsx
@@ -1,9 +1,13 @@
 import React from 'react';
 import { namespacedElementFactory } from '../../common/util/svg';
+import { LicenseGroup } from './LicenseGroup';
+import { DocumentMetadata } from '../DocumentMetadata';
 
 const svgNamespaceAttributes = {
   'xmlns:sodipodi': 'http://sodipodi.sourceforge.net/DTD/sodipodi-0.dtd',
   'xmlns:inkscape': 'http://www.inkscape.org/namespaces/inkscape',
+  'xmlns:cc': 'http://creativecommons.org/ns#',
+  'xmlns:rdf': 'http://www.w3.org/1999/02/22-rdf-syntax-ns#',
   xmlns: 'http://www.w3.org/2000/svg',
 };
 
@@ -19,6 +23,8 @@ const namedviewAttributes = {
 
 export const SVGWrapper = ({ children, ...rest }) => (
   <svg {...rest} {...svgNamespaceAttributes}>
+    <DocumentMetadata />
+    <LicenseGroup />
     <SodipodiNamedview {...namedviewAttributes} />
     {children}
   </svg>
